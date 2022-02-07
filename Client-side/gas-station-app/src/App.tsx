2fe43f3537e5180f4e0156f24f8938fcf4318362
boutheinakaroui <boutheina.karoui@etu.unice.fr>
2022-02-07 21:19:18 +0100

    map fixed

--- Client-side/gas-station-app/src/App.tsx
+++ Client-side/gas-station-app/src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { MapContainer, TileLayer, Popup, Polyline } from 'react-leaflet'
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <MapContainer center={[42.585444, 13.257684]} zoom={6} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+          <Popup position={[42.585444, 13.257684]}>
+            <div>
+              <h2>italie</h2>
+            </div>
+          </Popup>
+    </MapContainer>
   );
 }
 
