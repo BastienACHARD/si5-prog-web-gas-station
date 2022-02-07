4cfaf911c05da27e35b11cf181a4200ffc7b2b73
boutheinakaroui <boutheina.karoui@etu.unice.fr>
2022-02-07 17:39:15 +0100

    add map with some problems

--- Client-side/gas-station-app/src/App.tsx
+++ Client-side/gas-station-app/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './App.css';
+import {  TileLayer, Marker, Polyline,MapContainer} from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps | Readonly<AppProps>) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    const center = { lat: 51.721547, lng: -0.164687 };
+    const zoom = 8;
+    const positions = [
+    { lat: 51.44737, lng: -0.336682 },
+    { lat: 51.674019, lng: 0.093973 },
+    { lat: 51.907979, lng: -0.208439 },
+  ];
+
+    return (
+      <MapContainer center={center} zoom={zoom}>
+      <TileLayer
+        attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /><Polyline positions={positions} /></MapContainer>
+    );
+  }
 }
 
 export default App;
