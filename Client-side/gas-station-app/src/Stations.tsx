83ff1113d23406ebafec53a740bc538a97e85efb
boutheina.karoui <boutheina.karoui@etu.unice.fr>
2022-02-20 22:18:08 +0100

    add markers leaflet

---
+++ Client-side/gas-station-app/src/Stations.tsx
@@ -0,0 +1,13 @@
+
+
+export default class Station {
+    _id: number;
+    _latitude: number;
+    _longitude: number;
+    _adresse: string;
+    _ville: string;
+    _valeur:number;
+    _nom: string;
+
+
+}
