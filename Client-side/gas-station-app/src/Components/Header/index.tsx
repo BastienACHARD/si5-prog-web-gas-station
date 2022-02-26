d7de28b3cde8cbf6f9f8c825aaf389e49fe97160
BastienACHARD <bastien.achard@live.fr>
2022-02-26 16:31:18 +0100

    feat: Ajout des thèmes (Dark / Light)

---
+++ Client-side/gas-station-app/src/Components/Header/index.tsx
@@ -0,0 +1,33 @@
+import { FC, useContext} from 'react';
+import Switch from 'react-switch';
+import { shade } from 'polished';
+
+import { Container } from './styles';
+import { ThemeContext } from 'styled-components';
+
+interface Props {
+    toggleTheme(): void;
+}
+
+export const Header: FC<Props> = ({ toggleTheme }) => {
+    const { colors, title } = useContext(ThemeContext);
+
+    return (
+        <Container>
+            Notre App
+
+            <Switch
+            onChange={toggleTheme}
+            checked={title === 'dark'}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            height={10}
+            width={40}
+            handleDiameter={20}
+            offColor={shade(0.15, colors.primary)}
+            onColor={colors.secondary}
+            />
+        </Container>
+    );
+
+};
---
+++ Client-side/gas-station-app/src/Styles/Themes/light.tsx
@@ -0,0 +1,12 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#96C3CE',
+        secondary: '#4BC6B9',
+        
+        background: '#F5F5F5',
+        text: '#333'
+    },
+
+};
