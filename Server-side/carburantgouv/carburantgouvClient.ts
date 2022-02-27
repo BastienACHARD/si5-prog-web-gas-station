e3284a0e8f09ec468d0d35dc15ea7909970a0967
Lucas <lucas.aubron@etu.unice.fr>
2022-02-27 17:27:44 +0100

    No longer fetching gouv data

--- Server-side/carburantgouv/carburantgouvClient.ts
+++ Server-side/carburantgouv/carburantgouvClient.ts
@@ -9,11 +9,9 @@ import { fuels_name } from '../utils/fuels_name';
 import { todayDateAsNumber } from '../utils/dateAsNumber';
 
 async function fetchFromGouv(){
-
     const today = todayDateAsNumber();
 
     console.log("Updating mongo data with government data")
-    /*
     // récupérer le .zip du gouvernement correspondant aux dernières données
     try {
         fetch('https://donnees.roulez-eco.fr/opendata/instantane')
@@ -26,7 +24,6 @@ async function fetchFromGouv(){
 
     // dezip
     await unzip('zip_xml_file/PrixCarburants_instantane.zip');
-    */
     // xml => json, puis transformation des données en Array de l'objet "Station"
     const jsonContent = await xmlToJson(`zip_xml_file/PrixCarburants_instantane.xml`);
     const stations = gouvJsonToStation(jsonContent);
@@ -36,10 +33,10 @@ async function fetchFromGouv(){
     
     // insérer les nouvelles données
     //await insertMongo(JSON.stringify(stations));
-
+    
     // supprimer les données de prix du jour
     await dropPriceHistory(today);
-
+    
     // insérer les données de prix du jour
     await insertPriceHistory(today, averagePrice(stations, fuels_name));
 
